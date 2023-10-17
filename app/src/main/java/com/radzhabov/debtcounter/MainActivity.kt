e9bde942a58524207d4423b74ecb70bd369c006d
MoriatY <nacud85@gmail.com>
2023-10-17 15:49:05 +0300

    Initial commit

---
+++ app/src/main/java/com/radzhabov/debtcounter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.radzhabov.debtcounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import com.radzhabov.debtcounter.ui.theme.DebtCounterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DebtCounterTheme {
+                Text("Home Screen")
+            }
+        }
+    }
+}
